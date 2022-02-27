ad8c8e738913d544c4147b056cf01116bdac1fe5
Shen <shushen2013@gmail.com>
2022-02-27 02:01:27 -0500

    feat: add topbar and bottom wiht mui customize pallet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,24 @@ import { Route, Routes } from "react-router-dom";
 import Amplify from "aws-amplify";
 import AuctionsRouter from "./pages/auctions/AuctionsRouter";
 import AuthRouter from "./pages/auth/AuthRouter";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
-import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import MuiTheme from "./theme/MuiTheme";
+import Topbar from "./components/topbar/Topbar";
 import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
 
 export default function App() {
   return (
-    <>
-      <ResponsiveAppBar />
+    <MuiTheme>
+      <Topbar />
       <Routes>
         <Route exact path="" element={<Home />} />
         <Route path="auctions/*" element={<AuctionsRouter />} />
         <Route path="auth/*" element={<AuthRouter />} />
       </Routes>
-    </>
+      <Footer />
+    </MuiTheme>
   );
 }
