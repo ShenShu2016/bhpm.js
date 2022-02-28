fbb515f40c64ccfdc4180dd181dc270188e23d13
Shen <shushen2013@gmail.com>
2022-02-27 20:51:27 -0500

    feat: update custom-policies let cognito-idp allow

--- frontend/src/pages/auth/AuthRouter.js
+++ frontend/src/pages/auth/AuthRouter.js
@@ -2,14 +2,14 @@ import "@aws-amplify/ui-react/styles.css";
 
 import { Route, Routes } from "react-router-dom";
 
-//import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 import LoginPage from "./login";
 import React from "react";
 
 export default function AuthRouter() {
   return (
     <Routes>
-      {/* <Route exact path="" element={<Authenticator />} /> */}
+      <Route exact path="" element={<Authenticator />} />
       <Route exact path="/login" element={<LoginPage />} />
     </Routes>
   );
