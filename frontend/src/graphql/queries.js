9cdb3c9a26bf2d47685b5f0d350581e9751285f6
Shen <shushen2013@gmail.com>
2022-03-24 23:51:59 -0400

    update: bakend api

--- frontend/src/graphql/queries.js
+++ frontend/src/graphql/queries.js
@@ -1,6 +1,51 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const getProfile = /* GraphQL */ `
+  query GetProfile($id: ID!) {
+    getProfile(id: $id) {
+      id
+      name
+      companyName
+      address
+      fax
+      idPassport
+      title
+      phone
+      phone2
+      email
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listProfiles = /* GraphQL */ `
+  query ListProfiles(
+    $filter: ModelProfileFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listProfiles(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        companyName
+        address
+        fax
+        idPassport
+        title
+        phone
+        phone2
+        email
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
 export const getAuctionUserLimitation = /* GraphQL */ `
   query GetAuctionUserLimitation($id: ID!) {
     getAuctionUserLimitation(id: $id) {
