77cc8ab6f76593312064f026d51e8826bc8184a0
Shen <shushen2013@gmail.com>
2022-04-13 14:23:06 -0400

    fix: rename ProfileCreater to ProfileCreator

--- frontend/src/pages/profile/ProfileRouter.js
+++ frontend/src/pages/profile/ProfileRouter.js
@@ -2,16 +2,16 @@ import "@aws-amplify/ui-react/styles.css";
 
 import { Route, Routes } from "react-router-dom";
 
-import Profile from "./Profile";
-import ProfileCreater from "./ProfileCreater";
 import MyLot from "./MyLot";
+import Profile from "./Profile";
+import ProfileCreator from "./ProfileCreator";
 import React from "react";
 
 export default function ProfileRouter() {
   return (
     <Routes>
       <Route exact path="" element={<Profile />} />
-      <Route exact path="/create" element={<ProfileCreater />} />
+      <Route exact path="/create" element={<ProfileCreator />} />
       <Route exact path="/myLot" element={<MyLot/>} />
     </Routes>
   );
