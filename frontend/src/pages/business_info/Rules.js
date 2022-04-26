eea6f3f1b5bc0eaa222367e17fc17d43416c951b
Shen <shushen2013@gmail.com>
2022-04-26 18:54:32 -0400

    fix: remove useless api call

--- frontend/src/pages/business_info/Rules.js
+++ frontend/src/pages/business_info/Rules.js
@@ -1,26 +1,10 @@
 import { Box, Container, styled } from "@mui/material";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 
 import Navbar from "../../components/navbar/Navbar";
-import { fetchLotss } from "../../redux/slice/lotsSlice";
-
-//import Section12 from "../../components/superstore-shop/Section12";
-
+import React from "react";
 
 export default function AboutUs() {
-  const dispatch = useDispatch();
-  const { isAuthenticated } = useSelector((state) => state.userAuth);
-  useEffect(() => {
-    if (isAuthenticated !== null) {
-      dispatch(
-        fetchLotss({
-          isAuthenticated,
-          auctionsID: "fdfa3b15-1731-43fc-b23e-30c1707b954c",
-        })
-      );
-    }
-  }, [dispatch, isAuthenticated]);
+
 
   const InnerContainer = styled(Container)(() => ({
     display: "flex",
@@ -91,8 +75,6 @@ export default function AboutUs() {
           <br /><br />
         </Box>
       </InnerContainer>
-
-      {/* <Section12 /> */}
     </>
   );
 }
